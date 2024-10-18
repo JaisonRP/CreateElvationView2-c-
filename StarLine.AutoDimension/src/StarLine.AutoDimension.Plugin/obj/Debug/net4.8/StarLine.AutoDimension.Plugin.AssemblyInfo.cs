078a5d22f288f8c2670be36d517fd247cae4541e
Neel Shah <nshah@starlinewindows.com>
2024-10-18 11:48:13 -0700

    removed old gitignore file

--- StarLine.AutoDimension/src/StarLine.AutoDimension.Plugin/obj/Debug/net4.8/StarLine.AutoDimension.Plugin.AssemblyInfo.cs
+++ StarLine.AutoDimension/src/StarLine.AutoDimension.Plugin/obj/Debug/net4.8/StarLine.AutoDimension.Plugin.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StarLine.AutoDimension.Plugin")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.5.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12a995c37ee7039575fc4e1606881b5cd3ad96c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc81f68788c5f74328c2ee65a6faa98a953f55f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("StarLine.AutoDimension.Plugin")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StarLine.AutoDimension.Plugin")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.5.0.0")]
